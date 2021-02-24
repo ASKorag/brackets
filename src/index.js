339fe89d9415e1fd216735b3165f344f0f3a0407
Andrey Serikov <AS.Korag@protonmail.com>
2021-02-24 15:50:43 +0300

    feat: realize brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = check = (str, bracketsConfig) => {
+  let newStr = str
+
+  const escapedChars = '[()|'
+
+  let isIdentity
+
+  do {
+    isIdentity = false
+
+    for (paar of bracketsConfig) {
+      const stringForRegExp = paar
+        .map(char =>
+          escapedChars.includes(char) === true ? `\\${char}` : char
+        )
+        .join('')
+
+      const regExp = new RegExp(stringForRegExp, 'g')
+
+      if (regExp.test(newStr)) {
+        isIdentity = true
+        newStr = newStr.replace(regExp, '')
+      }
+    }
+  } while (isIdentity)
+
+  return newStr.length === 0
 }
